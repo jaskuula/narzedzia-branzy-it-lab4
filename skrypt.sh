fb89a8e9b4d81fc659d418b6d18d8185b015936c
jaskuula <mateqgames@gmail.com>
2025-04-27 08:29:54 +0200

    --logs z obsluga parametru

---
+++ skrypt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ "$1"== "--logs" ]]; then
+    num=${2:-100}
+    if [[ -n "$2" && "$2" =~ ^[0-9]+$ ]]; then
+        num=$2
+    fi
+    for ((i=1; i<=num; i++)); do
+        echo "Nazwa pliku: log${i}.txt" > "log${i}.txt"
+        echo "Nazwa skryptu: $0" >> "log${i}.txt"
+        echo "Data: $(date)" >> "log${i}.txt"
+    done
+fi
