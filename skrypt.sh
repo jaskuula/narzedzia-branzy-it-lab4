8d2d1672cf4932a4ca13281a8d54b0ab54d329ab
jaskuula <mateqgames@gmail.com>
2025-04-27 08:55:15 +0200

    Merge branch 'myslnik-8'

--- skrypt.sh
+++ skrypt.sh
@@ -27,3 +27,15 @@ if [[ "$1" == "--init" ]]; then
     export PATH=$PATH:$(pwd)/$repo_name
     echo "Sklonowano repozytorium i dodano do PATH"
 fi
+
+if [[ "$1" == "--error" || "$1" == "-e" ]]; then
+    num=${2:-100}
+    if [[ -n "$2" && "$2" =~ ^[0-9]+$ ]]; then
+        num=$2
+    fi
+    for ((i=1; i<=num; i++)); do
+        echo "Nazwa pliku: error${i}.txt" > "error${i}.txt"
+        echo "Nazwa skryptu: $0" >> "error${i}.txt"
+        echo "Data: $(date)" >> "error${i}.txt"
+    done
+fi
