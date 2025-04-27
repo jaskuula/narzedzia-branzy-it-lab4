193474294eb85c230cd0d6bbb1fea5cb79e48ee9
jaskuula <mateqgames@gmail.com>
2025-04-27 08:26:33 +0200

    dodano --logs oraz gitignore

---
+++ skrypt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ "$1"== "--logs" ]]; then
+    num=${2:-100}
+    for ((i=1; i<=num; i++)); do
+        echo "Nazwa pliku: log${i}.txt" > "log${i}.txt"
+        echo "Nazwa skryptu: $0" >> "log${i}.txt"
+        echo "Data: $(date)" >> "log${i}.txt"
+    done
+fi
